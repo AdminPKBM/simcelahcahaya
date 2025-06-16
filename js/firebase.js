b82ed5646f4781cd113103d82caa9803c456c7ef
AdminPKBM <122655136+AdminPKBM@users.noreply.github.com>
2025-06-16 13:06:06 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,10 @@
-// js/firebase.js
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "NAMA_PROJECT.firebaseapp.com",
-  projectId: "NAMA_PROJECT",
-  storageBucket: "NAMA_PROJECT.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "APP_ID"
+  apiKey: "AIzaSyBM-bS7ZY7BRM14c08UBX2RMRaU-DDeaps",
+  authDomain: "pkbm-celah-cahaya.firebaseapp.com",
+  projectId: "pkbm-celah-cahaya",
+  storageBucket: "pkbm-celah-cahaya.firebasestorage.app",
+  messagingSenderId: "714777975642",
+  appId: "1:714777975642:web:9edcb345070f7cfeeeb163",
+  measurementId: "G-SCRVW8SQQG"
 };
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
